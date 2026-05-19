d3acf3cc65ab25d5f2bf65bbea9155b7f72a4524
leonajosephine <leored@gmx.de>
2026-05-19 22:51:36 +0200

    add utils and types for product

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+    id: number;
+    slug: string;
+    name: string;
+    category: string;
+    material: string;
+    price: string;
+    description: string;
+    image: string;
+    hoverImage?: string;
+    gallery: string[];
+  };
